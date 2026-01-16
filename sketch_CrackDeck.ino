56e2b67cdeaec2a54207eed021396ee83cd450f8
Kurt <Standfan69@gmail.com>
2026-01-16 17:53:39 +0800

    init message

---
+++ sketch_CrackDeck.ino
@@ -0,0 +1,28 @@
+#include <Keypad.h>
+
+const byte ROWS = 4, COLS = 4;
+
+char keys[ROWS][COLS] = {
+  {'1','2','3','C'},
+  {'4','5','6','D'},
+  {'7','8','9','B'},
+  {'*','0','#','A'}
+
+};
+
+byte rowPins[ROWS] = {2,3,4,5};
+byte colPins[COLS] = {6,7,8,9};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup(){ 
+  Serial.print("Keypad Ready");
+  Serial.begin(9600); 
+
+  }
+void loop(){
+
+  char k = keypad.getKey();
+  if(k) Serial.println(k);
+  
+}
